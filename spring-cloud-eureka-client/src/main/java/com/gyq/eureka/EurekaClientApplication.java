4d1202213a85fb0de5f5e727de6fd06b864de65e
gaoyaqiu <30494613@qq.com>
2018-04-12 13:03:21 +0800

    1.服务的注册与发现（Eureka）

---
+++ spring-cloud-eureka-client/src/main/java/com/gyq/eureka/EurekaClientApplication.java
@@ -0,0 +1,25 @@
+package com.gyq.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @auther gaoyaqiu
+ */
+@RestController
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String index() {
+        return "Hello World!";
+    }
+}
