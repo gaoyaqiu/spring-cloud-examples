5ab01fd336bd618db22302d7a166d7c59e7b66e0
gaoyaqiu <30494613@qq.com>
2018-04-16 14:09:53 +0800

    4.断路器监控(Hystrix Dashboard)

--- spring-cloud-ribbon-consumer-hystrix-dashboard/src/main/java/com/gyq/dashboard/HystrixDashBoardApplication.java
+++ spring-cloud-ribbon-consumer-hystrix-dashboard/src/main/java/com/gyq/dashboard/HystrixDashBoardApplication.java
@@ -1,5 +1,7 @@
 package com.gyq.dashboard;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -10,6 +12,8 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -20,9 +24,28 @@ import org.springframework.web.client.RestTemplate;
 @EnableCircuitBreaker
 @EnableEurekaClient
 //@EnableTurbine
+@RestController
 public class HystrixDashBoardApplication {
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HystrixDashBoardApplication.class, args);
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "defaultStores")
+    @GetMapping("/hello")
+    public String hello() {
+        return restTemplate.getForEntity("http://eureka-client/hello", String.class).getBody();
+    }
+
+    public String defaultStores() {
+        return "Ribbon + hystrix + dashboard ,提供者服务挂了";
     }
 }
